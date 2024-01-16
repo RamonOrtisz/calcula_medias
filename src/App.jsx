b348c81edecaf88ffbb359cc726a8bf80f6d0930
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2024-01-16 17:06:29 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Perfil from "./Components/Perfil";
+import Formulario from "./Components/Formulario";
+
+function App() {
+  return (
+    <>
+      <Perfil nome="Ramon Galdino" endereco="https://github.com/RamonOrtisz.png"/>
+      <Formulario />
+    </>
+  )
+}
+
+export default App
